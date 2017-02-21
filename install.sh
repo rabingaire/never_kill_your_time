c06b46e7b3aab51003366508eb66ea9114a55445
Rabin Gaire <rabingaire20@gmail.com>
2017-02-21 12:27:35 +0545

    small changes on install

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 cp never_kill_your_time /bin/never_kill_your_time
 chmod a+x /bin/never_kill_your_time
 echo "Thank you for installing."
-echo "Please add this program to startup application to get full out of it."
-echo "Path this program is installed is on /bin/never_kill_your_time"
+echo "Please add this program to startup application to get the full out of it."
+echo "This program is installed on /bin/never_kill_your_time"
